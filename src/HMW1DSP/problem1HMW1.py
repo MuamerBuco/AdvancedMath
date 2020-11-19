169dbf3a69cabcb9354a91d7df79b3a188794d3c
MuamerBuco <muamerbuco@gmail.com>
2020-11-19 21:13:44 +0100

    Finished first problem (A)

--- src/HMW1DSP/problem1HMW1.py
+++ src/HMW1DSP/problem1HMW1.py
@@ -12,21 +12,19 @@ amplitude and sampling frequency.
 B.	Use a three-sample method for doing the same job in part A.
 
 V(t) = Vm * sin(wt * Qv)
-V(t) = 220 * sin(2*pi*50 + 30)
-
 x = A^-1 * b
 '''
 
 t_arr = np.array([[0.012,0.018], [0.014,0.016], [0.02,0.05],
-[0.011,0.013], [0.005,0.01], [0.01,0.03]]) # time array of arbitrary size
-f_arr = np.array([50, 60, 120, 240, 360, 400]) # frequency array
+[0.011,0.013], [0.005,0.01], [0.01,0.015]]) # time array of arbitrary size
+f_arr = np.array([50, 60, 120, 240, 360, 80]) # frequency array
 Vm_arr = np.array([110, 220, 380, 460, 620, 880]) # amplitude array
 Qv_arr = np.array([mt.radians(30), mt.radians(60), mt.radians(20), # phase shift arrays
-mt.radians(30), mt.radians(30), mt.radians(30)])
+mt.radians(30), mt.radians(50), mt.radians(40)])
 
 def printVariables(**args):
     for key, value in args.items():
-        print("Value " , key , " = " , value)
+        print(key , " = " , value)
 
 # first argument is omega(w), the rest is an arbitrary
 # number of time instances
@@ -43,6 +41,8 @@ def runSimulation(version):
     Vm = Vm_arr[version]
     Qv = Qv_arr[version]
 
+    w = 2*mt.pi*f
+
     def createSine():
         time = np.linspace(0,0.1,1000)
         voltage = np.zeros([1000])
@@ -54,20 +54,17 @@ def runSimulation(version):
     # get an arbitrary number of voltage samples
     def sampleVoltages(*args):
         V = []
-        for Q in args:
-            Vnext = (Vm*mt.sin(Q)*mt.cos(Qv) + Vm*mt.sin(Qv)*mt.cos(Q))
-            V.append(Vm*mt.sin(Q)*mt.cos(Qv) + Vm*mt.sin(Qv)*mt.cos(Q))
+        for t in args:
+            V.append(Vm * mt.sin(w*t + Qv))
         return V
     
-    w = 2*mt.pi*f
-
     createSine()
 
     # calculate theta1 and theta2
     Q = calculateQ(w, t[0], t[1])
 
     # get 2 voltage samples
-    V = sampleVoltages(Q[0], Q[1])
+    V = sampleVoltages(t[0], t[1])
 
     # some linear algebra
     Q12 = np.array([[mt.sin(Q[0]), mt.cos(Q[0])], 
@@ -82,7 +79,9 @@ def runSimulation(version):
     Vmv = VmQv[1]/mt.sin(Qvv)
 
     # print everything
-    printVariables(Q = Q, V = V, VmQv = VmQv, Qvv = mt.degrees(Qvv), Vmv = Vmv)
+    printVariables(frequency = f, Amplitude = Vm, PhaseShift = mt.degrees(Qv), Qs = Q,
+     SampledVoltages = V, VmQv = VmQv, CalculatedPhaseShift = mt.degrees(Qvv), 
+     CalculatedAmplitude = Vmv)
 
-runSimulation(0)
+runSimulation(5)
 # %%
